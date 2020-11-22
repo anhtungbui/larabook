5257a72f4457aa67b5b4ede64ca4c610615108f4
Anh Tung Bui <anhtung.bui@gmail.com>
2020-11-22 23:27:28 +0100

    Add Profile model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Model relationships
+     */
+    public function profile()
+    {
+        $this->hasOne(Profile::class);
+    }
 }
---
+++ database/migrations/2020_11_22_215608_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('avatar_src')->default('/avatars/avatar-placeholder.png');
+            $table->string('bio')->nullable();
+            $table->string('website')->nullable();
+            $table->string('location')->nullable();
+            $table->string('hometown')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
